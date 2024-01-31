8c9a8c20638dc730d0962c74cc92beeffb0da1e4
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-01-30 22:41:21 -0800

    Cleans up debugging puts lines

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -7,22 +7,17 @@ class FlightsController < ApplicationController
     @arrival_airports = Airport.all.map{ |airport| airport.code }
 
     if search_submitted?
-      puts "here"
       @flights = Flight.where(
         departure_airport_id: params[:departure_airport_id],
         arrival_airport_id: params[:arrival_airport_id]).where(
           "departure_time > ?", params[:departure_date]
         )
-        puts "hi"
-        puts @flights.count
-
     end
 
   end
 
 
   def search_submitted?
-    puts params
     params[:departure_airport_id] && params[:arrival_airport_id] && params[:departure_date] && params[:num_passengers]
   end
 end
