55cb46734d2bf37b9c92be00ff4989ff1fd1dadc
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-01-30 22:24:51 -0800

    Adds in logic to flights#index to allow for the search form submission to be sent back and retrieved

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,28 @@
 class FlightsController < ApplicationController
+
+  helper_method :search_submitted?
+
   def index
     @departure_airports = Airport.all.map{ |airport| airport.code }
     @arrival_airports = Airport.all.map{ |airport| airport.code }
+
+    if search_submitted?
+      puts "here"
+      @flights = Flight.where(
+        departure_airport_id: params[:departure_airport_id],
+        arrival_airport_id: params[:arrival_airport_id]).where(
+          "departure_time > ?", params[:departure_date]
+        )
+        puts "hi"
+        puts @flights.count
+
+    end
+
+  end
+
+
+  def search_submitted?
+    puts params
+    params[:departure_airport_id] && params[:arrival_airport_id] && params[:departure_date] && params[:num_passengers]
   end
 end
