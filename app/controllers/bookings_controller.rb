c0c7defda1fb3236096337ed5ddccd6356302c62
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-02-05 17:40:25 -0800

    Adds in nested form and buttons

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,6 +2,7 @@ class BookingsController < ApplicationController
 
   def new
     @flight = Flight.find(params[:flight_id])
-    @booking = Booking.new(flight_id: @flight_id, no_of_passengers: params[:no_of_passengers])
+    @booking = Booking.new(flight_id: @flight_id)
+    params[:no_of_passengers].to_i.times { @booking.passengers.build }
   end
 end
