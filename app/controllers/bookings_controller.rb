a5852daa85762f507c0367ac55d968c64e442bd5
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-02-05 17:55:03 -0800

    Modifies booking controller to have create booking successfully

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,4 +5,20 @@ class BookingsController < ApplicationController
     @booking = Booking.new(flight_id: @flight_id)
     params[:no_of_passengers].to_i.times { @booking.passengers.build }
   end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking.id)
+    else
+      @flight = Flight.find(params[:booking][:flight_id])
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:no_of_passengers, :flight_id, passengers_attributes: [:id, :name, :email])
+  end
 end
