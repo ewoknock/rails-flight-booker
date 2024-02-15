99befa5b6820dd226f69152b8c8236a518b98b0a
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-02-14 17:12:28 -0800

    Implements add/remove buttons to function correctly for adding new passengers on booking page

--- app/javascript/controllers/passenger_controller.js
+++ app/javascript/controllers/passenger_controller.js
@@ -2,6 +2,30 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="passenger"
 export default class extends Controller {
+
+  static targets = ["passengerFields", "passengerInfo", "passengerOutput", "passengerHeader"]
+  static values = { passengerCount: Number }
   connect() {
+    console.log(this.passengerCountValue)
+  }
+
+  add(e){
+    e.preventDefault;
+
+    this.passengerCountValue++;
+    const content = this.passengerFieldsTarget.innerHTML.replace(/NEW_RECORD/g, new Date().getTime().toString());
+    this.passengerOutputTarget.insertAdjacentHTML('beforeend', content);
+    this.passengerHeaderTargets[this.passengerCountValue-1].textContent = `Details for passenger ${this.passengerCountValue}`;
+
+    console.log(this.passengerInfoTargets);
+  }
+
+  delete(){
+    if(this.passengerCountValue > 1){
+      this.passengerCountValue--;
+      this.passengerInfoTargets[this.passengerCountValue].remove();
+      //this.passengerInfoTarget.remove();
+    }
+    console.log(this.passengerInfoTargets);
   }
 }
